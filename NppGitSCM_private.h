929412cf3f7abda7c8b7f7abffea743740e8e9d3
mvincent <mvincent@mitre.org>
2018-11-15 08:05:55 -0500

    Changed name to GitSCM (drop Npp prefix) for new N++ 7.6 plugin compatibility.

--- NppGitSCM_private.h
+++ NppGitSCM_private.h
@@ -1,23 +1,23 @@
 /* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
 /* DO NOT EDIT ! */
 
-#ifndef NPPGIT_PRIVATE_H
-#define NPPGIT_PRIVATE_H
+#ifndef NPPGITSCM_PRIVATE_H
+#define NPPGITSCM_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"1.2.1.1"
-#define VER_MAJOR	1
-#define VER_MINOR	2
-#define VER_RELEASE	1
-#define VER_BUILD	1
-#define COMPANY_NAME	"Vin's World"
-#define FILE_VERSION	VER_STRING
-#define FILE_DESCRIPTION	"Notepad++ Plugin for Git SCM."
-#define INTERNAL_NAME	"Notepad++ Git SCM"
-#define LEGAL_COPYRIGHT	"Copyright (C) VinsWorld. All Rights Reserved."
-#define LEGAL_TRADEMARKS	""
-#define ORIGINAL_FILENAME	""
-#define PRODUCT_NAME	"NPPGITSCM"
-#define PRODUCT_VERSION	VER_STRING
+#define VER_STRING  "1.2.1.1"
+#define VER_MAJOR   1
+#define VER_MINOR   2
+#define VER_RELEASE 1
+#define VER_BUILD   1
+#define COMPANY_NAME      "Vin's World"
+#define FILE_VERSION      VER_STRING
+#define FILE_DESCRIPTION  "Notepad++ Plugin for Git SCM."
+#define INTERNAL_NAME     "Notepad++ Git SCM"
+#define LEGAL_COPYRIGHT   "Copyright (C) VinsWorld. All Rights Reserved."
+#define LEGAL_TRADEMARKS  ""
+#define ORIGINAL_FILENAME ""
+#define PRODUCT_NAME      "GITSCM"
+#define PRODUCT_VERSION   VER_STRING
 
-#endif /*NPPGIT_PRIVATE_H*/
+#endif /*NPPGITSCM_PRIVATE_H*/
