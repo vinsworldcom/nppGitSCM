cb1964ae7abbf97adae56258a1b3374831b11d8e
mvincent <mvincent@mitre.org>
2019-03-21 14:37:37 -0400

    Tortoise phase 3 - panel checkbox sync'd with menu

--- NppGitSCM.cpp
+++ NppGitSCM.cpp
@@ -64,17 +64,17 @@ extern "C" __declspec( dllexport ) FuncItem *getFuncsArray( int *nbF )
 
 extern "C" __declspec( dllexport ) void beNotified( SCNotification *notifyCode )
 {
-	switch (notifyCode->nmhdr.code)
-	{
-	case NPPN_SHUTDOWN:
-	{
-		commandMenuCleanUp();
-	}
-	break;
+    switch (notifyCode->nmhdr.code)
+    {
+    case NPPN_SHUTDOWN:
+    {
+        commandMenuCleanUp();
+    }
+    break;
 
-	default:
-		return;
-	}
+    default:
+        return;
+    }
 }
 
 
