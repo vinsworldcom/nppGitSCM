c8d3b6e5bc2c2641d0599c10885ecd97f45f27cd
VinsWorldcom <vin@vinsworld.com>
2023-12-19 17:27:30 -0500

    bump version

--- resource.h
+++ resource.h
@@ -7,8 +7,8 @@
 /* VERSION DEFINITIONS */
 #define VER_MAJOR   1
 #define VER_MINOR   4
-#define VER_RELEASE 8
-#define VER_BUILD   3
+#define VER_RELEASE 9
+#define VER_BUILD   1
 #define VER_STRING  STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_RELEASE) "." STR(VER_BUILD)
 
 #define FILE_DESCRIPTION  "Notepad++ Plugin for Git SCM."
